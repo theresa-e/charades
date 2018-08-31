7029ba6366d86778ef796674e2ddfc45d9bea36e
Theresa Eliger <910theresa@gmail.com>
2018-08-30 18:43:36 -0700

    Added express-session and minor updates to routes.

--- server.js
+++ server.js
@@ -13,10 +13,24 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// ---------- Session ----------
+var session = require('express-session');
+app.use(session({
+    secret: 'SecretSessionCode',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 600000
+    }
+}))
+
+
 /* ---------- socket.io ---------- */
 const io = require('socket.io')(server);
 var host = {}
 var room;
+
+// Establish connection with client
 io.on('connection', function (socket) {
     socket.on('host', (data) => {
         host = data;
@@ -26,11 +40,10 @@ io.on('connection', function (socket) {
         // Socket will join room.
         socket.join(room);
         room = room
-        console.log('@@@@@@@@@@@@@@@@@@: ', room);
     })
 })
-
 io.sockets.in(room).emit('message', 'hay yallllllllllllll');
+
 /*  ---------- View Engine ---------- */
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
@@ -41,24 +54,30 @@ app.use(express.static(__dirname + '/static'));
 // Start new game form is submitted.
 app.post('/new-game', (req, res) => {
     // get username
-    console.log(req.body);
+    console.log('NEW GAME, REQ.BODY: ', req.body);
 
-    // generate a random code for their game
+    // generate a random code for a new game
     var gameCode = '';
     var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (var i=0; i<6; i++) {
+    for (var i = 0; i < 6; i++) {
         gameCode += characters.charAt(Math.floor(Math.random() * characters.length));
     }
+
+    // custom path for each game
     res.redirect('/game/' + gameCode);
 });
 
-app.get('/game/:code', (req, res) => {
+app.get('/game/:gameCode', (req, res) => {
     console.log('REQ.BODY: ', req.body)
-    console.log('The game code: ', req.params.code);
-    res.render('game');
+    console.log('The game code: ', req.params.gameCode);
+
+    res.render('game', {
+        name: req.body
+    });
 });
 
-// Land ing page
+// Landing
 app.get('/', (req, res) => {
+    var name = req.body;
     res.send('index.html');
 });
