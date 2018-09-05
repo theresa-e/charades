d83f6c6f8eeed8799202d5200eeed8d673abfc5f
Theresa Eliger <910theresa@gmail.com>
2018-09-04 18:10:54 -0700

    Changed server files to fix room functionality, added EJS get values in game view.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var path = require("path")
 
-/* ---------- Express ---------- */
+// ---------- Express ---------- 
 const express = require('express');
 const app = express();
 const server = app.listen(7000, function () {
@@ -9,7 +9,7 @@ const server = app.listen(7000, function () {
 });
 console.log("Running at port 7000...");
 
-/* ---------- Body Parser ---------- */
+// ---------- Body Parser ---------- 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -27,94 +27,67 @@ app.use(session({
     }
 }))
 
-/* ---------- socket.io ---------- */
+// ---------- socket.io ---------- 
 const io = require('socket.io')(server);
 
 // temp variables for scope
 var tempUser;
+var usernames = {};
+var rooms = [];
+
 io.sockets.on('connection', function (socket) {
-    var allMsgs = [];
-    var roomName;
-    var user = tempUser
 
+    if (tempUser) {
+        socket.nickname = tempUser.name // save name in socket connection
+    }
+    console.log('Socket successfully connected, id: ', socket.id);
     socket.on('room', function (room) {
-        console.log('room name: ', room)
+        rooms.push(room);
         roomName = room;
-        socket.join(room);
-
-        // Get all existing messages in the room
-        socket.emit('getAllMsgs', {
-            allMsgs: allMsgs
-        });
-
-        if (user) {
-            if (user.isHost){
-                console.log('--- New game has started: ', user);
-            } else {
-                console.log('--- Someone joined a game: ', user)
-            }
-        }
-        
-        socket.on('message', (res) => {
-            console.log('new message received from server :', res)
-            allMsgs.push(res.message);
-            io.sockets.in(roomName).emit('new-message-added', {
-                message: res.message
-            })
-        });
-
-        socket.on('video', (res) => {
-            console.log('RES----: ', res)
-        })
+        socket.join(roomName);
+        socket.room = roomName;
+        console.log('---- JOINING ROOM: ', roomName)
     });
 });
 
-
-/*  ---------- View Engine ---------- */
+// ---------- View Engine ----------
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 // ---------- Static Views ----------
 app.use(express.static(__dirname + '/static'));
 
-
 // 'Start new game' form is submitted
-app.post('/new', (req, res) => {
-    if (req.body.hostName) {
-        // generate a random code for a new game
-        var gameCode = '';
-        var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for (var i = 0; i < 6; i++) {
-            gameCode += characters.charAt(Math.floor(Math.random() * characters.length));
-        }
-    
-        // save gameCode and name to emit to client
-        roomName = gameCode;
-        tempUser = {
-            name: req.body.hostName,
-            isHost: true
-        };
-    
-        // custom path for each game
-        res.redirect('/game/' + gameCode);
-    } else {
-        req.flash('newgameMessage', 'You must enter a name to start a new game.');
-        res.redirect(301, '/');
+app.get('/new', (req, res) => {
+    console.log('------ New game request!')
+    // generate a random code for a new game
+    var gameCode = '';
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 6; i++) {
+        gameCode += characters.charAt(Math.floor(Math.random() * characters.length));
     }
+
+    // save gameCode and name to emit to client
+    roomName = gameCode;
+    req.session.isHost = true;
+
+    // custom path for each game
+    res.redirect('/game/' + gameCode);
 });
 
 // 'Join existing game' form is submitted
-app.post('/join', (req, res) => {
+app.get('/join', (req, res) => {
     console.log('------ Request to join game: ', req.body);
-    tempUser = req.body.user
+    req.session.isHost = false; // let user join as a player
     res.redirect('/game/' + req.body.gameCode);
 });
 
 // Route for game page
 app.get('/game/:roomCode', (req, res) => {
-    console.log(req.params.roomCode);
+    var userInfo = req.session.isHost; // user joins as a HOST!
     res.render('game', {
-        roomName: req.params.roomCode
+        userInfo: userInfo,
+        roomCode: req.params.roomCode
     });
 });
 
